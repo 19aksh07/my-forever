21b48a6a1bf6df3c1c3e63254f89e26308878cba
TR Akshay <akshaytr19.8@outlook.com>
2025-06-19 00:06:26 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    base: mode === 'development' ? '/' : '/my-forever/',
   },
   plugins: [
     react(),
