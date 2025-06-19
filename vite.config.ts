265fcf33bde330c5911e92f89f1431be44ecf0ac
TR Akshay <akshaytr19.8@outlook.com>
2025-06-19 07:06:00 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
-// vite.config.js or vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const repoName = "my-forever"; 
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`, 
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  base:"/my-forever",
+  plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
